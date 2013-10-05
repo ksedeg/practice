05e8753a3b9085c4e3672d086c0ee36cef41109c
Ksenia Degteva <ksedeg@cs.spbstu.ru>
2013-10-05 19:09:20 +0400

    Corrected dictionary

--- practice_2/monthconv.py
+++ practice_2/monthconv.py
@@ -1,9 +1,9 @@
 def MonthConv():
-	dmonth={"January":1,"February":2,"March":3,"April":4,"May":5, "June":6,
-		"July":7, "August":8, "September":9, "October":10, 
-		"November":11, "December":12}
+	dmonth={"january":1,"february":2,"march":3,"april":4,"may":5, "june":6,
+		"july":7, "august":8, "september":9, "october":10, 
+		"november":11, "december":12}
 	m=raw_input("Please, enter month: ")
-	m=m[0].upper()+m[1:].lower()
+	m=m.lower()
 	if dmonth.has_key(m)==True:
 		print("Month number: %s" %dmonth[m])
 	else:
