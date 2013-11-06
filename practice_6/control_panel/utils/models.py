a90e1c5364d666b0e2cf41633f23dc9828d70e37
root <root@ksedeg-VirtualBox.(none)>
2013-11-07 00:05:22 +0400

    Corrected travis errors

--- practice_6/control_panel/utils/models.py
+++ practice_6/control_panel/utils/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 import datetime
 
-# Create your models here.
+
 class TimeStampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True, default=datetime.datetime.now())
     modified = models.DateTimeField(auto_now=True, default=datetime.datetime.now())
 
 
-    class Meta:
-        abstract = True
+class Meta:
+    abstract = True
